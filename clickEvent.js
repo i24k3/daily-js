7e9dcd9529f0c6b3f67bacecb7d488e48b992ff5
Arsalan Khan (i24k3) <akha3743@gmail.com>
2025-01-07 07:03:29 +0000

    add new feature for onlyjs, eventListner();

--- clickEvent.js
+++ clickEvent.js
@@ -1,12 +1,32 @@
 
 
-const div = document.createElement('div');
-function style(target, prop, value) {
+
+function style(target = document.body, prop, value) {
   return target.style[prop] = value;
 }
+
+function eventListener(target, eventType, method) {
+  return target.addEventListener(eventType, method);
+}
+
+const div = document.createElement('div');
 
 style(div, 'backgroundColor', 'lightblue');
 style(div, 'width', '77px');
 style(div, 'height', '77px');
 
 document.body.appendChild(div);
+
+eventListener(div, 'mouseover', function () {
+  setTimeout(() => {
+  style(div, 'backgroundColor', 'lightpink');
+  style(div, 'delay', '3');
+  }, 70);
+});
+
+eventListener(div, 'mouseout', function () {
+  setTimeout(() => {
+  style(div, 'backgroundColor', 'lightblue')
+  style(div, 'delay', '3');
+  }, 70);
+});
